3e766e5d9a74c469cd0332adb5acf810a647dd91
noticeapp <akibrotsamuelas@gmail.com>
2021-12-06 14:27:44 -0500

    delete cors

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
 
 app.listen(process.env.PORT || 7070, () => {
   console.log("app started");
