e8427824097226a194975860271e0c90b117622c
noticeapp <akibrotsamuelas@gmail.com>
2021-12-06 14:59:33 -0500

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import router from "./router/router.js";
-
+import cors from 'cors'
 const app = express();
 app.use(express.json({ limit: "50mb" }));
-
+app.use(cors())
 app.listen(process.env.PORT || 7070, () => {
   console.log("app started");
 });
