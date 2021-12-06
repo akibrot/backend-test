7ad5d413ee751c2d350ab89e107afc3fe9b50a76
noticeapp <akibrotsamuelas@gmail.com>
2021-12-06 15:08:16 -0500

    add package mongoose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./router/router.js";
 import cors from 'cors'
+import mongoose  from "mongoose";
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors())
